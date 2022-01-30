dc5f5a6d2b3e5ac567f32d4cce0d41c3ff445a32
StakeOut92 <inozemtcew@gmail.com>
2022-01-31 00:23:30 +0300

    Added tests to my own Web page

---
+++ src/test/java/MySiteOutputTest.java
@@ -0,0 +1,137 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.NoAlertPresentException;
+import org.openqa.selenium.NoSuchElementException;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedCondition;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.Assert;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+import java.util.Set;
+
+public class MySiteOutputTest {
+    public static WebDriver driver;
+
+    @BeforeMethod
+    public void beforeMethod() {
+        System.setProperty("webdriver.chrome.driver", "src/test/resources/chromedriver.exe");
+        driver = new ChromeDriver();
+        //Open start page
+        driver.get("file:///D:\\учеба\\TeachMeSkills\\Body-weight-calculator-tests\\src\\test\\resources\\index.html");
+    }
+
+    @AfterMethod
+    public void afterMethod() {
+        driver.quit();
+    }
+
+    @Test
+    public void findHeaderMessageTest() {
+        //Find Header message
+        try {
+            boolean headerMessageIsDisplayed = driver.findElement(By.id("header")).isDisplayed();
+            Assert.assertTrue(headerMessageIsDisplayed, "Message is displayed");
+        } catch (NoSuchElementException e) {
+            Assert.fail("Header message is not display");
+        }
+    }
+
+    @Test
+    public void findWithNameMessageTest() {
+        //Find Name message
+        try {
+            boolean headerMessageIsDisplayed = driver.findElement(By.id("header2")).isDisplayed();
+            Assert.assertTrue(headerMessageIsDisplayed, "Message is displayed");
+        } catch (NoSuchElementException e) {
+            Assert.fail("Message with name is not display");
+        }
+    }
+
+    @Test
+    public void favouriteMusicBandListEnableTest() {
+        //Find list with music bands
+        try {
+            boolean favouriteMusicBandListIsEnable = driver.findElement(By.id("band")).isDisplayed();
+            Assert.assertTrue(favouriteMusicBandListIsEnable, "List is enable");
+        } catch (NoSuchElementException e) {
+            Assert.fail("List with music band is not enable");
+        }
+    }
+
+    @Test
+    public void clickMeButtonIsEnableTest() {
+        //Find button 'Click me,please'
+        try {
+            boolean clickMeButtonIsEnable = driver.findElement(By.id("elem")).isEnabled();
+            Assert.assertTrue(clickMeButtonIsEnable, "Click me button is enable");
+        } catch (NoSuchElementException e) {
+            Assert.fail("Click me button is not enable");
+        }
+    }
+
+    @Test
+    public void afterClickMeButtonAlertWindowIsEnableTest() {
+        //Click button 'Click me,please' and check alert window
+        try {
+            driver.findElement(By.id("elem")).click();
+            driver.switchTo().alert().accept();
+        } catch (NoSuchElementException e) {
+            Assert.fail("Click me button is not enable");
+        } catch (NoAlertPresentException e) {
+            Assert.fail("Alert window is not enable");
+        }
+    }
+
+    @Test
+
+    public void alertMessageTextIsCorrectTest() {
+        //Click button 'Click me,please' and check alert window text
+        try {
+            driver.findElement(By.id("elem")).click();
+            String message = driver.switchTo().alert().getText();
+            Assert.assertEquals(message, "SAAB is one of the best car ever", "Alert message is false");
+            driver.switchTo().alert().accept();
+
+        } catch (NoSuchElementException e) {
+            Assert.fail("Click me button is not enable");
+        } catch (NoAlertPresentException e) {
+            Assert.fail("Alert window is not enable");
+        }
+    }
+
+    @Test
+    public void checkActiveLinkOnPageTest() {
+        //Search active link on page
+        boolean activeLink = driver.findElement(By.id("link1")).isEnabled();
+        Assert.assertTrue(activeLink, "Active link is enable");
+        //
+    }
+
+    @Test
+    public void checkActiveLinkURLIsTrueTest() {
+        Set<String> oldWindowsSet = driver.getWindowHandles();
+        driver.findElement(By.id("link1")).click();
+        String newWindowHandle = (new WebDriverWait(driver, 10))
+                .until(new ExpectedCondition<String>() {
+                           public String apply(WebDriver driver) {
+                               Set<String> newWindowsSet = driver.getWindowHandles();
+                               newWindowsSet.removeAll(oldWindowsSet);
+                               return newWindowsSet.size() > 0 ?
+                                       newWindowsSet.iterator().next() : null;
+                           }
+                       }
+                );
+        String openedWindowURL = driver.switchTo().window(newWindowHandle).getCurrentUrl();
+        Assert.assertEquals(openedWindowURL, "http://www.saabclub.by/");
+    }
+
+    @Test
+    public void checkOoopsYouFoundItMessageIsEnableTest(){
+        String ooopsMessage = driver.findElement(By.id("image1")).getAttribute("alt");
+        Assert.assertEquals(ooopsMessage,"oooops,you found it");
+
+    }
+}
